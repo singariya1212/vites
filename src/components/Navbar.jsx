ab6fed7606071d12805bb3b78189e52258f2b7a9
hemant <singariya1212@gmail.com>
2025-01-30 11:40:05 +0530

    merge prak

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,8 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="#">Navbarsss</a>
+      <h1>kk</h1>
    <span>ttotal Users {newCount}</span>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
